dcb63b02d089a865100cfaf8d173cd65ae816801
Asam-Prathyusha <asam.p@saksoft.com>
2025-11-10 10:52:29 +0530

    KAN-20 Added Footer Component

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,11 +3,13 @@ import { injectSpeedInsights } from '@vercel/speed-insights';
 import { TaskCreateComponent } from "./features/task-mangement/components/task-create-component/task-create-component";
 import { RouterOutlet } from '@angular/router';
 import { TasksViewComponent } from "./features/task-mangement/components/tasks-view-component/tasks-view-component";
+import { FooterComponent } from './shared/components/footer-component/footer-component';
+import { SidebarComponent } from "./shared/components/sidebar-componen/sidebar-component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [TaskCreateComponent, RouterOutlet, TasksViewComponent],
+  imports: [TaskCreateComponent, TasksViewComponent, FooterComponent, SidebarComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
