7d1e46513d4a1b368bed1761b238e77cb9ee114e
BPriyanka229 <priyanka.sh@saksoft.com>
2025-11-13 19:10:36 +0530

    KAN-19 updated shared components name

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
-import { FooterComponent } from './shared/components/footer-component/footer-component';
+import { FooterComponent } from './shared/components/footer.component/footer.component';
 
 @Component({
   selector: 'app-root',
