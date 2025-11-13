5c579906654b94f71019e1bd848c6a976819b479
Pragathi-S-Hegde <pragathi.s@saksoft.com>
2025-11-13 14:23:36 +0530

    KAN-5 Initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,5 @@
 import { Routes } from '@angular/router';
-import { RegisterComponent } from './features/user-account-management/components/register-component/register-component';
-
-
+import { RegisterComponent } from './features/user-account-management/components/register/register.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
