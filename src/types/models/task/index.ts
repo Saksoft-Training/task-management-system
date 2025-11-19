5830ae10a126cf6c35db109139b3469dee766e8d
BPriyanka229 <priyanka.sh@saksoft.com>
2025-11-19 16:12:06 +0530

    KAN-19 notifications

--- src/types/models/task/index.ts
+++ src/types/models/task/index.ts
@@ -1,5 +1,18 @@
 export type TaskStatus = 'To Do' | 'In Progress' | 'Completed';
 export type TaskPriority = 'Low' | 'Medium' | 'High' | 'Urgent';
 export interface Task {
-    id: string;
+  id: number;
+  title: string;
+  description?: string;
+  status: TaskStatus;
+  priority: TaskPriority;
+  assignee: string;       
+  assigneeEmail: string;  
+  dueDate: string;
+  projectId: number;
+  createdBy: string;
+  createdAt: string;
+  updatedAt: string;
+  completedAt?: string | null;
+
 }
