a7abbf6a90fe55c2893c3b16f298cae8c2bcaa80
BPriyanka229 <priyanka.sh@saksoft.com>
2025-11-19 19:17:55 +0530

    KAN-17 display statistics completed

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -10,11 +10,12 @@ import { LoginComponent } from './features/user-account-management/components/lo
 import { ForgotPasswordComponent } from './features/user-account-management/components/forgot-password/forgot-password.component';
 import { ProfileComponent } from './features/project-management/components/profile/profile.component';
 import { ProjectListComponent } from './features/project-management/components/project-list/project-list.component';
+import { TaskCreateComponent } from './features/task-management/components/tast-create.component/task-create.component';
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
-
+  {path:'tasks',component:TaskCreateComponent},
   {path:'dashboard-comp',component:DashboardComponent},
     { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
