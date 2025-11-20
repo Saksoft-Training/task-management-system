30c68f528648025673d5f79f3fcb24a2db545975
BPriyanka229 <priyanka.sh@saksoft.com>
2025-11-20 12:22:27 +0530

    KAN-17-displaying-statistics-and-overview

--- src/types/models/statistics/index.ts
+++ src/types/models/statistics/index.ts
@@ -1,11 +1,11 @@
 export interface Statistics {
-totalProjects: number;
-activeProjects: number;
-completedProjects: number;
-totalTasks: number;
-completedTasks: number;
-inProgressTasks: number;
-overdueTasks: number;
-overallCompletionRate: number;
-updatedAt: string;
+    totalProjects: number;
+    activeProjects: number;
+    completedProjects: number;
+    totalTasks: number;
+    completedTasks: number;
+    inProgressTasks: number;
+    overdueTasks: number;
+    overallCompletionRate: number;
+    updatedAt: string;
 }
