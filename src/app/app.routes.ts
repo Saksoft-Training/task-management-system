67750eb80feaa9d2cbc210c23e0b8df20f991938
BPriyanka229 <priyanka.sh@saksoft.com>
2025-11-20 18:42:43 +0530

    KAN-17-display-statistics-and-overview

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,7 +16,7 @@ export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'register', component: RegisterComponent },
   {path:'tasks',component:TaskCreateComponent},
-  {path:'dashboard',component:DashboardComponent},
+  {path:'dashboard-stats',component:DashboardComponent},
     { path: 'login', component: LoginComponent },
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'projects/create', component: ProjectCreateComponent },
