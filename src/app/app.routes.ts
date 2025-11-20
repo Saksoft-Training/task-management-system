4217ab71be9c3560ce768bf12d7fd6116fc1e064
keerthipraveen2405 <a.keerthireddy@gmail.com>
2025-11-20 19:31:40 +0530

    KAN-22 Build issue solved

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -11,7 +11,7 @@ import { ProjectListComponent } from './features/project-management/components/p
 import { TaskDetailPageComponent } from './features/task-management/components/task-detail-page.component/task-detail-page.component';
 import { ProfileComponent } from './features/user-account-management/components/profile/profile.component';
 import { ProjectDetailComponent } from './features/project-management/components/project-detail/project-detail.component';
-import { TaskCreateComponent } from './features/task-management/components/tast-create.component/task-create.component';
+
 
 export const routes: Routes = [
   { path: '', redirectTo: 'register', pathMatch: 'full' },
