f5882e08efea8b12ccaef3bded40dfeea0d000ed
BPriyanka229 <priyanka.sh@saksoft.com>
2025-11-21 16:17:06 +0530

    KAN-18-visual-charts-and-activity-feed

--- src/types/models/task/index.ts
+++ src/types/models/task/index.ts
@@ -1,11 +1,12 @@
-
-
 export type TaskPriority = 'High' | 'Low' | 'Medium' | 'Urgent';
 export type TaskStatus = 'Completed' | 'In Progress' | 'To Do';
- 
+
 export interface Task {
-  id: number;
-  title: string;
+  assignee: string;
+  assigneeEmail: string;
+  completedAt?: string | null;
+  createdAt: string;
+  createdBy: string;
   description?: string;
   dueDate: string;
   id: number;
@@ -14,6 +15,6 @@ export interface Task {
   status: TaskStatus;
   title: string;
   updatedAt: string;
+
 }
- 
- 
+
