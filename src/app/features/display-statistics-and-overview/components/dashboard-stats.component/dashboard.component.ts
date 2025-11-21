a6c5a0b1fbb22e1c808f171fe63a209cb46470e2
BPriyanka229 <priyanka.sh@saksoft.com>
2025-11-21 23:19:20 +0530

    KAN-18

--- src/app/features/display-statistics-and-overview/components/dashboard-stats.component/dashboard.component.ts
+++ src/app/features/display-statistics-and-overview/components/dashboard-stats.component/dashboard.component.ts
@@ -4,11 +4,14 @@ import { Statistics } from '../../../../../types/models/statistics';
 import { DashboardService } from '../../services/dashboard-service';
 import { StatisticsCardComponent } from '../statistics-card.component/statistics-card.component';
 import { CommonModule } from '@angular/common';
+import { ActivityFeedComponent } from '../../../dashboard/components/activity-feed.component/activity-feed.component';
+import { ChartComponent } from '../../../dashboard/components/chart.component/chart.component';
+import { DashboardChartsComponent } from '../../../dashboard/components/dashboard-charts.component/dashboard-charts.component';
 
 @Component({
   selector: 'app-dashboard.component',
   standalone: true,
-  imports: [StatisticsCardComponent, CommonModule],
+  imports: [StatisticsCardComponent, CommonModule,ActivityFeedComponent,ChartComponent,DashboardChartsComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
