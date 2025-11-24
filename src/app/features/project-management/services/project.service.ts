0a9a57d8ae17c8dcb7e69d086259dd2263cdf59b
Pragathi-S-Hegde <pragathi.s@saksoft.com>
2025-11-24 16:46:56 +0530

    KAN-9

--- src/app/features/project-management/services/project.service.ts
+++ src/app/features/project-management/services/project.service.ts
@@ -88,5 +88,3 @@ export class ProjectService {
   }
   // #endregion
 }
-
-
--- src/types/models/project/index.ts
+++ src/types/models/project/index.ts
@@ -1,17 +1,12 @@
 export interface Project {
-
-
     createdAt: string;
     createdBy: string;
     description?: string;
     endDate: string;
-
     id: number;
     name: string;
     startDate: string;
     status: 'Planning' | 'In Progress' | 'Completed' | 'On Hold';
-
-
     updatedAt: string;
 
 }
